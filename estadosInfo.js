1655e25426c6d709e5557a013d8675412188341a
RhysUBA <comisionesrhys@gmail.com>
2025-10-22 10:38:34 -0400

    feat: completar información de todos los estados venezolanos en estadosInfo.js
    - Se agregaron los 13 estados faltantes: Distrito Capital, Falcón, Guárico, Lara,
      Mérida, Miranda, Monagas, Nueva Esparta, Portuguesa, Sucre, Táchira, Trujillo,
      Vargas, Yaracuy y Zulia
    - Cada estado incluye: capital, imagen, elementos culturales, sitios turísticos
      y datos curiosos
    - Se mantuvo la estructura de datos existente y formato consistente
    - Todas las imágenes son de Wikimedia Commons para uniformidad

--- estadosInfo.js
+++ estadosInfo.js
@@ -1,4 +1,3 @@
-
 const estadosInfo = {
   "Amazonas": {
     capital: "Puerto Ayacucho",
@@ -169,6 +168,291 @@ const estadosInfo = {
       "Región compuesta por miles de caños y ríos",
       "El idioma warao es hablado por miles de personas",
       "Importante reserva ecológica"
+    ]
+  },
+  "Distrito Capital": {
+    capital: "Caracas",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Distrito_Capital_in_Venezuela.svg/300px-Distrito_Capital_in_Venezuela.svg.png",
+    cultura: [
+      "Festival de la Virgen de la Divina Pastora",
+      "Quema de Judas",
+      "Gastronomía criolla: arepas, pabellón"
+    ],
+    turismo: [
+      "Panteón Nacional",
+      "Parque Nacional El Ávila",
+      "Museo de Bellas Artes"
+    ],
+    datosCuriosos: [
+      "Sede de los poderes públicos nacionales",
+      "Fundada en 1567 por Diego de Losada",
+      "Cuna de Simón Bolívar"
+    ]
+  },
+  "Falcón": {
+    capital: "Coro",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Falcon_in_Venezuela.svg/300px-Falcon_in_Venezuela.svg.png",
+    cultura: [
+      "Tambor coriano",
+      "Fiesta de los Tambores de San Juan",
+      "Artesanía en arcilla"
+    ],
+    turismo: [
+      "Parque Nacional Médanos de Coro",
+      "Península de Paraguaná",
+      "Ciudad de Coro (Patrimonio de la Humanidad)"
+    ],
+    datosCuriosos: [
+      "Tiene el único desierto de Venezuela",
+      "Primera capital histórica de Venezuela",
+      "Posee importantes refinerías de petróleo"
+    ]
+  },
+  "Guárico": {
+    capital: "San Juan de los Morros",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Guarico_in_Venezuela.svg/300px-Guarico_in_Venezuela.svg.png",
+    cultura: [
+      "Joropo central",
+      "Toros coleados",
+      "Festival del Joropo Recio"
+    ],
+    turismo: [
+      "Parque Nacional Aguaro-Guariquito",
+      "Monumento al Cristo de los Morros",
+      "Aguas termales de San Juan"
+    ],
+    datosCuriosos: [
+      "Corazón de los llanos centrales",
+      "Mayor productor de arroz del país",
+      "Tiene formaciones rocosas únicas"
+    ]
+  },
+  "Lara": {
+    capital: "Barquisimeto",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Lara_in_Venezuela.svg/300px-Lara_in_Venezuela.svg.png",
+    cultura: [
+      "Tamunangue (baile folklórico)",
+      "Feria Internacional de Barquisimeto",
+      "Gastronomía: olleta de gallo, majarete"
+    ],
+    turismo: [
+      "Monumento al Sol Naciente",
+      "Parque Nacional Yacambú",
+      "Jardín Botánico de Barquisimeto"
+    ],
+    datosCuriosos: [
+      "Conocida como la capital musical de Venezuela",
+      "Cuna de compositores como Alí Primera",
+      "Cuarto estado más poblado del país"
+    ]
+  },
+  "Mérida": {
+    capital: "Mérida",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Merida_in_Venezuela.svg/300px-Merida_in_Venezuela.svg.png",
+    cultura: [
+      "Feria del Sol",
+      "Festival de la Virgen de la Inmaculada Concepción",
+      "Gastronomía andina: pizca andina, arepas de trigo"
+    ],
+    turismo: [
+      "Teleférico de Mérida (más alto y largo del mundo)",
+      "Pico Bolívar",
+      "Parque Nacional Sierra Nevada"
+    ],
+    datosCuriosos: [
+      "Tiene las montañas más altas de Venezuela",
+      "Ciudad universitaria por excelencia",
+      "Fundada en 1558"
+    ]
+  },
+  "Miranda": {
+    capital: "Los Teques",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Miranda_in_Venezuela.svg/300px-Miranda_in_Venezuela.svg.png",
+    cultura: [
+      "Diablos Danzantes de Yare (Patrimonio de la Humanidad)",
+      "Parranda de San Pedro",
+      "Gastronomía: pan de jamón, hallacas"
+    ],
+    turismo: [
+      "Playas de Barlovento",
+      "Parque Nacional El Ávila",
+      "Colonia Tovar"
+    ],
+    datosCuriosos: [
+      "Segundo estado más poblado de Venezuela",
+      "Tiene importante diversidad climática",
+      "Cuna del prócer Francisco de Miranda"
+    ]
+  },
+  "Monagas": {
+    capital: "Maturín",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Monagas_in_Venezuela.svg/300px-Monagas_in_Venezuela.svg.png",
+    cultura: [
+      "Fiesta de la Cruz de Mayo",
+      "Velorios de Cruz",
+      "Gastronomía: casabe, sancocho de guaraguara"
+    ],
+    turismo: [
+      "Parque Nacional Cueva del Guácharo",
+      "Monumento al Samán de Güere",
+      "Aguas termales de Miraflores"
+    ],
+    datosCuriosos: [
+      "Importante zona petrolera",
+      "Tiene la cueva más grande de Venezuela",
+      "Lugar de nacimiento de José Tadeo Monagas"
+    ]
+  },
+  "Nueva Esparta": {
+    capital: "La Asunción",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Nueva_Esparta_in_Venezuela.svg/300px-Nueva_Esparta_in_Venezuela.svg.png",
+    cultura: [
+      "Fiesta de la Virgen del Valle",
+      "Danza del Carite",
+      "Tradiciones de pescadores"
+    ],
+    turismo: [
+      "Playas de Porlamar y Pampatar",
+      "Parque Nacional Laguna de La Restinga",
+      "Castillo de San Carlos de Borromeo"
+    ],
+    datosCuriosos: [
+      "Único estado insular de Venezuela",
+      "Formado por tres islas principales",
+      "Llamada 'Tierra de Gracia' por Colón"
+    ]
+  },
+  "Portuguesa": {
+    capital: "Guanare",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Portuguesa_in_Venezuela.svg/300px-Portuguesa_in_Venezuela.svg.png",
+    cultura: [
+      "Fiesta de la Virgen de Coromoto",
+      "Festival del Joropo Llanero",
+      "Gastronomía llanera"
+    ],
+    turismo: [
+      "Santuario Nacional de la Virgen de Coromoto",
+      "Parque Nacional Dinira",
+      "Río Guanare"
+    ],
+    datosCuriosos: [
+      "Capital espiritual de Venezuela",
+      "Importante productor agrícola",
+      "Lugar de la aparición de la Virgen de Coromoto"
+    ]
+  },
+  "Sucre": {
+    capital: "Cumaná",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Sucre_in_Venezuela.svg/300px-Sucre_in_Venezuela.svg.png",
+    cultura: [
+      "Fiesta de la Cruz de Mayo",
+      "Tambores de Cumaná",
+      "Gastronomía basada en mariscos"
+    ],
+    turismo: [
+      "Parque Nacional Mochima",
+      "Castillo de San Antonio de la Eminencia",
+      "Playas del Golfo de Cariaco"
+    ],
+    datosCuriosos: [
+      "Primera ciudad fundada en América del Sur (1515)",
+      "Cuna del Gran Mariscal de Ayacucho",
+      "Tiene importantes reservas de gas natural"
+    ]
+  },
+  "Táchira": {
+    capital: "San Cristóbal",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Tachira_in_Venezuela.svg/300px-Tachira_in_Venezuela.svg.png",
+    cultura: [
+      "Feria Internacional de San Sebastián",
+      "Gastronomía andina: pizca andina, mute",
+      "Música andina"
+    ],
+    turismo: [
+      "Páramo El Zumbador",
+      "Pueblo de San Pedro del Río",
+      "Parque Nacional Chorro El Indio"
+    ],
+    datosCuriosos: [
+      "Fronterizo con Colombia",
+      "Cuna de varios presidentes venezolanos",
+      "Región cafetalera por excelencia"
+    ]
+  },
+  "Trujillo": {
+    capital: "Trujillo",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Trujillo_in_Venezuela.svg/300px-Trujillo_in_Venezuela.svg.png",
+    cultura: [
+      "Fiesta de la Virgen de la Paz",
+      "Danza de los Pastores del Niño Jesús",
+      "Gastronomía: quesos andinos, miche"
+    ],
+    turismo: [
+      "Monumento a la Virgen de la Paz",
+      "Parque Nacional Dinira",
+      "Páramo de Guaramacal"
+    ],
+    datosCuriosos: [
+      "Tiene el monumento más alto de América (Virgen de la Paz)",
+      "Lugar donde se firmó el Decreto de Guerra a Muerte",
+      "Importante productor de café y cambur"
+    ]
+  },
+  "Vargas": {
+    capital: "La Guaira",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Vargas_in_Venezuela.svg/300px-Vargas_in_Venezuela.svg.png",
+    cultura: [
+      "Fiesta de San Juan Bautista",
+      "Tambores de la costa",
+      "Gastronomía a base de pescado"
+    ],
+    turismo: [
+      "Playas de Catia La Mar",
+      "Puerto de La Guaira",
+      "Camino de los Españoles"
+    ],
+    datosCuriosos: [
+      "Principal puerto de Venezuela",
+      "Creación reciente (1998)",
+      "Vulnerable a deslaves naturales"
+    ]
+  },
+  "Yaracuy": {
+    capital: "San Felipe",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Yaracuy_in_Venezuela.svg/300px-Yaracuy_in_Venezuela.svg.png",
+    cultura: [
+      "Fiesta de San Juan Bautista",
+      "Danza de la Sanjuanera",
+      "Gastronomía: casabe, dulces criollos"
+    ],
+    turismo: [
+      "Parque Nacional Yurubí",
+      "Monumento al Cacique Yaracuy",
+      "Centro de Artes La Sirena"
+    ],
+    datosCuriosos: [
+      "Llamado 'Tierra de María Lionza'",
+      "Importante zona agrícola",
+      "Tiene reservas minerales importantes"
+    ]
+  },
+  "Zulia": {
+    capital: "Maracaibo",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Zulia_in_Venezuela.svg/300px-Zulia_in_Venezuela.svg.png",
+    cultura: [
+      "Gaita zuliana",
+      "Feria de La Chinita",
+      "Danza de Las Turas"
+    ],
+    turismo: [
+      "Lago de Maracaibo",
+      "Puente General Rafael Urdaneta",
+      "Páramo El Rosal"
+    ],
+    datosCuriosos: [
+      "Principal región petrolera de Venezuela",
+      "Tiene el lago más grande de Sudamérica",
+      "Región con identidad cultural muy marcada"
     ]
   }
 };
